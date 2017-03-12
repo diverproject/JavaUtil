357bf55166ac142bd3c02a7233b2aaac1ded588f
Andrew Mello <driwundernight@gmail.com>
2017-03-12 05:01:20 -0300

    1.0.2.19 - Tornando os métodos zip e unzip mais dinâmicos através de opções por parâmetros. 	- Agora será possível definir o nível de compressão e tamanho do buffer.

--- Zip/src/org/diverproject/util/jzip/ZipUtil.java
+++ Zip/src/org/diverproject/util/jzip/ZipUtil.java
@@ -1,5 +1,7 @@
 package org.diverproject.util.jzip;
 
+import static org.diverproject.util.lang.IntUtil.limit;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -26,34 +28,72 @@ public class ZipUtil
 	public static boolean LOG = false;
 
 	/**
-	 * Compacta um determinado vetor de bytes em formato ZIP.
-	 * Definido como padrão com BEST_COMPRESSION e HUFFMAN_ONLY.
-	 * @param input dados que serão compactados.
-	 * @return vetor com os bytes compactados.
+	 * Procedimento que realiza a compactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e o tipo de compactação (influenciam na velocidade).
+	 * Neste caso <b>enableException</b> é desativado, ou seja não causa exception em caso de falha,
+	 * também é definido um tamanho padrão para o buffer interno utilizado (<code>BUFFER_SIZE</code>)
+	 * e o nível de compressão é o máximo disponível para este formato (9, BEST_COMPRESSION).
+	 * @param input vetor contendo os bytes do qual deseja compactar no formato ZIP.
+	 * @param compressionLevel nível de compressão, influencia na redução de bytes (1 a 9).
+	 * @param bufferSize tamanho do buffer interno que será usado para compactar os dados.
+	 * @return retorna o vetor contendo os bytes compactados em ZIP ou null se falhar.
 	 */
 
 	public static byte[] zip(byte input[])
 	{
-		return zip(input, false);
+		return zip(input, Deflater.BEST_COMPRESSION, BUFFER_SIZE, false);
 	}
 
 	/**
-	 * Compacta um determinado vetor de bytes em formato ZIP.
-	 * Definido como padrão com BEST_COMPRESSION e HUFFMAN_ONLY.
-	 * @param input vetor dos dados em bytes que serão compactados.
-	 * @param enableExpcetion habilitar o uso de UtilRuntimeException.
-	 * @return vetor com os bytes compactados.
+	 * Procedimento que realiza a compactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e o tipo de compactação (influenciam na velocidade).
+	 * Neste caso <b>enableException</b> é desativado, ou seja não causa exception em caso de falha,
+	 * também é definido um tamanho padrão para o buffer interno utilizado (<code>BUFFER_SIZE</code>).
+	 * @param input vetor contendo os bytes do qual deseja compactar no formato ZIP.
+	 * @param compressionLevel nível de compressão, influencia na redução de bytes (1 a 9).
+	 * @param bufferSize tamanho do buffer interno que será usado para compactar os dados.
+	 * @return retorna o vetor contendo os bytes compactados em ZIP ou null se falhar.
 	 */
 
-	public static byte[] zip(byte input[], boolean enableException)
+	public static byte[] zip(byte input[], int compressionLevel)
 	{
-		Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION);
+		return zip(input, compressionLevel, BUFFER_SIZE, false);
+	}
+
+	/**
+	 * Procedimento que realiza a compactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e o tipo de compactação (influenciam na velocidade).
+	 * Neste caso <b>enableException</b> é desativado, ou seja não causa exception em caso de falha.
+	 * @param input vetor contendo os bytes do qual deseja compactar no formato ZIP.
+	 * @param compressionLevel nível de compressão, influencia na redução de bytes (1 a 9).
+	 * @param bufferSize tamanho do buffer interno que será usado para compactar os dados.
+	 * @return retorna o vetor contendo os bytes compactados em ZIP ou null se falhar.
+	 */
+
+	public static byte[] zip(byte input[], int compressionLevel, int bufferSize)
+	{
+		return zip(input, compressionLevel, bufferSize, false);
+	}
+
+	/**
+	 * Procedimento que realiza a compactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e o tipo de compactação (influenciam na velocidade).
+	 * @param input vetor contendo os bytes do qual deseja compactar no formato ZIP.
+	 * @param compressionLevel nível de compressão, influencia na redução de bytes (1 a 9).
+	 * @param bufferSize tamanho do buffer interno que será usado para compactar os dados.
+	 * @param enableException true para causar exception em caso de falha ou false caso contrário.
+	 * @return retorna o vetor contendo os bytes compactados em ZIP ou null se falhar.
+	 */
+
+	public static byte[] zip(byte input[], int compressionLevel, int bufferSize, boolean enableException)
+	{
+		Deflater deflater = new Deflater(limit(compressionLevel, Deflater.BEST_SPEED, Deflater.BEST_COMPRESSION));
 		deflater.setStrategy(Deflater.FILTERED);
 		deflater.setInput(input);
 		deflater.finish();
 
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		byte[] buffer = new byte[BUFFER_SIZE];
+		byte[] buffer = new byte[bufferSize];
 
 		while (!deflater.finished())
 		{
@@ -75,30 +115,49 @@ public class ZipUtil
 	}
 
 	/**
-	 * Descompacta um determinado vetor de bytes supostamente em formato ZIP.
-	 * @param input vetor dos dados em bytes que serão descompactados.
-	 * @return vetor dom os bytes descompactados ou null se falhar.
+	 * Procedimento que realiza a descompactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e se este irá causar exception por má formação.
+	 * Neste caso <b>enableException</b> é desativado, ou seja não causa exception em caso de falha,
+	 * também é definido um tamanho padrão para o buffer interno utilizado (<code>BUFFER_SIZE</code>).
+	 * @param input vetor contendo os bytes do qual deseja descompactar do formato ZIP.
+	 * @return retorna o vetor contendo os bytes descompactados do formato ZIP ou null se falhar.
 	 */
 
 	public static byte[] unzip(byte input[])
 	{
-		return unzip(input, false);
+		return unzip(input, BUFFER_SIZE, false);
 	}
 
 	/**
-	 * Descompacta um determinado vetor de bytes supostamente em formato ZIP.
-	 * @param input vetor dos dados em bytes que serão descompactados.
-	 * @param enableExpcetion habilitar o uso de UtilRuntimeException.
-	 * @return vetor dom os bytes descompactados.
+	 * Procedimento que realiza a descompactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e se este irá causar exception por má formação.
+	 * Neste caso <b>enableException</b> é desativado, ou seja não causa exception em caso de falha.
+	 * @param input vetor contendo os bytes do qual deseja descompactar do formato ZIP.
+	 * @param bufferSize tamanho do buffer interno que será usado para descompactar os dados.
+	 * @return retorna o vetor contendo os bytes descompactados do formato ZIP ou null se falhar.
 	 */
 
-	public static byte[] unzip(byte input[], boolean enableException)
+	public static byte[] unzip(byte input[], int bufferSize)
+	{
+		return unzip(input, bufferSize, false);
+	}
+
+	/**
+	 * Procedimento que realiza a descompactação de um conjunto de bytes especificados no formato ZIP.
+	 * É permitido definir o tamanho do buffer e se este irá causar exception por má formação.
+	 * @param input vetor contendo os bytes do qual deseja descompactar do formato ZIP.
+	 * @param bufferSize tamanho do buffer interno que será usado para descompactar os dados.
+	 * @param enableException true para causar exception em caso de falha ou false caso contrário.
+	 * @return retorna o vetor contendo os bytes descompactados do formato ZIP ou null se falhar.
+	 */
+
+	public static byte[] unzip(byte input[], int bufferSize, boolean enableException)
 	{
 		Inflater inflater = new Inflater();
 		inflater.setInput(input);
 
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		byte buffer[] = new byte[BUFFER_SIZE];
+		byte buffer[] = new byte[bufferSize];
 
 		try {
 
